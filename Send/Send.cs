78fe5e48294e700575da6ef95f0df0806d80c8d1
adnanzaka <chadnanzaka@hotmail.com>
2022-09-07 15:07:34 +0400

    Rabbit MQ Queuing Implementation C#
    Rabbit MQ Queuing Implementation C#

---
+++ Send/Send.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            //var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            var factory = new ConnectionFactory() { HostName = "10.30.3.50",UserName="test",Password="test" };           
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "QUEUE_PREAPPROVAL",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "MB229558709AE";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "QUEUE_PREAPPROVAL",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+           // Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+        }
+    }
+}
